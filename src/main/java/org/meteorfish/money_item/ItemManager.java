6e7a6898e1fbd94714a9fdd58334a2951ff9a49d
mete0rfish <sungwon326@naver.com>
2025-02-04 21:54:37 +0900

    fix(withdrawal): 입금 설명 추가

--- src/main/java/org/meteorfish/money_item/ItemManager.java
+++ src/main/java/org/meteorfish/money_item/ItemManager.java
@@ -15,6 +15,8 @@ public class ItemManager {
     public final static int _5_K_AMOUNT = 5000;
     public final static int _1_K_AMOUNT = 1000;
 
+    private final static String WITHDRAW_DESCRIPTION = "입금하려면 허공에 [SHIFT + 우클릭]";
+
     private static ItemStack buildItem(Material material, int amount, String displayName, String... lore) {
         ItemStack stack = new ItemStack(material, amount);
         ItemMeta meta = stack.getItemMeta();
@@ -28,36 +30,36 @@ public class ItemManager {
             Material.DIAMOND,
             1,
             ChatColor.AQUA +"100,000 골드",
-            "100,000 골드 가치의 화폐",
-            ChatColor.LIGHT_PURPLE + "우린 부자가 될 거야!");
+            "우린 부자가 될 거야!",
+            ChatColor.LIGHT_PURPLE + WITHDRAW_DESCRIPTION);
 
     public static final ItemStack _50_K = buildItem(
             Material.GOLD_INGOT,
             1,
             ChatColor.GOLD +"50,000 골드",
             "50,000 골드 가치의 화폐",
-            ChatColor.LIGHT_PURPLE + "돈돈돈돈");
+            ChatColor.LIGHT_PURPLE + WITHDRAW_DESCRIPTION);
 
     public static final ItemStack _10_K = buildItem(
             Material.GOLD_INGOT,
             1,
             ChatColor.GOLD +"10,000 골드",
             "10,000 골드 가치의 화폐",
-            ChatColor.LIGHT_PURPLE + "만 골드의 행복");
+            ChatColor.LIGHT_PURPLE + WITHDRAW_DESCRIPTION);
 
     public static final ItemStack _5_K = buildItem(
             Material.GOLD_INGOT,
             1,
             ChatColor.GOLD +"5,000 골드",
             "5,000 골드 가치의 화폐",
-            ChatColor.LIGHT_PURPLE + "?????");
+            ChatColor.LIGHT_PURPLE + WITHDRAW_DESCRIPTION);
 
     public static final ItemStack _1_K = buildItem(
             Material.GOLD_INGOT,
             1,
             ChatColor.GOLD +"1,000 골드",
             "1,000 골드 가치의 화폐",
-            ChatColor.LIGHT_PURPLE + "?");
+            ChatColor.LIGHT_PURPLE + WITHDRAW_DESCRIPTION);
 
 
 }
